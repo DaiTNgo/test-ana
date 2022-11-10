20b31ad223a0781a83ae01c4a5239763d700bba2
daingo <daingo7211@gmail.com>
2022-11-10 13:30:24 +0700

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReactGA from 'react-ga';
@@ -39,13 +39,24 @@ function App() {
 					</a>
 				</p>
 			</header>
-
+			<A />
 			<button>Click Button 1</button>
 			<button>Click Button 2</button>
 			<button>Click Button 3</button>
 			<button>Click Button 4</button>
 		</div>
 	);
+}
+
+function A() {
+	useEffect(() => {
+		ReactGA.event({
+			category: 'User',
+			action: 'Created an Account',
+		});
+	}, []);
+
+	return <h1>hello</h1>;
 }
 
 export default App;
